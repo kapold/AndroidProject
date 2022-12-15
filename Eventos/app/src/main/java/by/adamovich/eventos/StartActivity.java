1c29d8312fa8bb71596f3145709e45c37ca160a4
kapold <antonadamovich@mail.ru>
2022-12-15 21:06:35 +0300

    Animations between fragments done

--- Eventos/app/src/main/java/by/adamovich/eventos/StartActivity.java
+++ Eventos/app/src/main/java/by/adamovich/eventos/StartActivity.java
@@ -9,18 +9,10 @@ import by.adamovich.eventos.fragments.RegisterFragment;
 
 public class StartActivity extends AppCompatActivity {
     public boolean fragmentSelection = false;
-//    ImageView loader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-//        loader = findViewById(R.id.imageView);
-//        CircularProgressDrawable spinner = new CircularProgressDrawable(this);
-//        spinner.setStrokeWidth(20f);
-//        spinner.setCenterRadius(200f);
-//        spinner.setColorSchemeColors(Color.parseColor("#C700FE"));
-//        spinner.start();
-//        Glide.with(this).load("").error(spinner).into(loader);
 
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
@@ -31,17 +23,30 @@ public class StartActivity extends AppCompatActivity {
     }
 
     public void changeFragments(){
-        // TODO: do animations
         if (fragmentSelection){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragmentContainerView_StartActivity, new AuthFragment()).commit();
+                    .setCustomAnimations(
+                            R.anim.slide_in,
+                            R.anim.fade_out,
+                            R.anim.fade_in,
+                            R.anim.slide_out
+                    )
+                    .replace(R.id.fragmentContainerView_StartActivity, new AuthFragment())
+                    .commit();
             fragmentSelection = false;
         }
         else{
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragmentContainerView_StartActivity, new RegisterFragment()).commit();
+                    .setCustomAnimations(
+                            R.anim.slide_in,
+                            R.anim.fade_out,
+                            R.anim.fade_in,
+                            R.anim.slide_out
+                    )
+                    .replace(R.id.fragmentContainerView_StartActivity, new RegisterFragment())
+                    .commit();
             fragmentSelection = true;
         }
     }
--- Eventos/app/src/main/java/by/adamovich/eventos/databases/SQLiteHandler.java
+++ Eventos/app/src/main/java/by/adamovich/eventos/databases/SQLiteHandler.java
@@ -7,6 +7,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SQLiteHandler extends SQLiteOpenHelper {
     private static final String DB_NAME = "NotesDB";
     private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME_NOTES = "Notes";
+
+    // Notes
+    private static final String ID_NOTE_COL = "idNote";
+    private static final String CREATOR_COL = "creator";
+    private static final String TITLE_COL = "title";
+    private static final String TEXT_COL = "noteText";
+    private static final String DATE_COL = "lastDateEdit";
 
     public SQLiteHandler(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
